ee914bd02f14252f47f02677d0e4efc9c044ffff
Marco Bonomo <marco.bonomo@gmail.com>
2016-11-05 02:04:55 +0100

    lets clean up a bit, shall we?

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,4 @@
 
 $router->define([
     '' => 'App\Controllers\HomeController',
-    'about' => 'controllers/about.php',
-    'about/culture' => 'controllers/about-culture.php',
-    'contact' => 'controllers/contact.php'
 ]);
