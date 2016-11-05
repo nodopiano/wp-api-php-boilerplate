8594f70ae38619ce2cf49b1eaf0ef9501ee6c8ac
Marco Bonomo <marco.bonomo@gmail.com>
2016-11-05 18:51:28 +0100

    twig views, master layout and index.html

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -4,20 +4,15 @@ Namespace App\Controllers;
 use App\Core\Controller;
 
 /**
-*
-*/
+ *
+ */
 class HomeController extends Controller
 {
 
-	public function show() {
+    public function show() {
 
-		$page_url = file_get_contents('http://www.luovo.bio/wp-json/wp/v2/pages/61');
-		$page = json_decode($page_url);
-		$campi = array('txtimg_img', 'double_img', 'slide_image');
-		foreach ($page->acf->page as $block) {
-			$this->functions::getMedia($block, $campi);
-		}
-		return $this->twig->loadTemplate('index.html')->render(array('data' => $page));
+        // $page = $this->api->pages(61);
+        return $this->twig->loadTemplate('index.html')->render(array('message' => 'Hello!'));
 
-	}
+    }
 }
