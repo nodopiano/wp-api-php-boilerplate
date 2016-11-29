b8c33d76c3f74e4b773f023e8ca95539756c55ab
Marco Bonomo <marco.bonomo@gmail.com>
2016-11-29 10:38:38 +0100

    back on corda router! (actually, where using a wrapper)

--- app/boot.php
+++ app/boot.php
@@ -3,18 +3,11 @@ Namespace App;
 
 require '../vendor/autoload.php';
 Use Nodopiano\Corda\Bootstrap;
-use Phroute\Phroute\RouteCollector;
-use Phroute\Phroute\Dispatcher;
+use Nodopiano\Corda\Router;
 use Nodopiano\Corda\Request;
 use Nodopiano\Corda\App;
 
 Bootstrap::boot(__dir__);
 
-$router = new RouteCollector();
-require '../app/routes.php';
-$router->get('{catchAll}', ['Nodopiano\Corda\Controller','notFound']);
-
-$dispatcher = new Dispatcher($router->getData());
-$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-// Print out the value returned from the dispatched function
+$response = (new Router)->load('../app/routes.php')->direct();
 $response->render();
