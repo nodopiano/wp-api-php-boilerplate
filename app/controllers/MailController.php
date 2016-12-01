0994f8d92dd3472fac03faf9ec40ef0a1b47c94a
Marco Bonomo <marco.bonomo@gmail.com>
2016-12-01 11:53:09 +0100

    export to csv, subscribe to mailchimp

--- app/controllers/MailController.php
+++ app/controllers/MailController.php
@@ -14,10 +14,8 @@ class MailController extends Controller
 
     public function send()
     {
-
       // Csv::export(array($_POST['from'],$_POST['message']));
       // NewsLetter::subscribe('38e73a2904',array('email' => $_POST['from']));
-
       return json(Mail::send($_POST['from'],$_POST['message']));
     }
 }
