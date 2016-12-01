ea44bfeb84f474e3c4693eebbbd6a47dbc8f2c76
Marco Bonomo <marco.bonomo@gmail.com>
2016-12-01 11:52:21 +0100

    laravel Envoy for fast deployments

---
+++ Envoy.blade.php
@@ -0,0 +1,25 @@
+@servers(['production' => ''])
+
+@setup
+$user = '';
+$domain = '';
+$repository = '';
+@endsetup
+
+@task('clone', ['on' => 'production'])
+  cd /home/{{$user}}/web/{{$domain}}/public_html/
+  git clone {{$repository}} .
+  cp .env-example .env
+  composer install
+@endtask
+
+@task('deploy', ['on' => 'production'])
+  cd /home/{{$user}}/web/{{$domain}}/public_html/
+  git checkout -f master
+  git pull
+  composer install
+@endtask
+
+
+
+
