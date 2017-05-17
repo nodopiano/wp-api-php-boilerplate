0470a8b8f9c2b5b24c6cea0ee24392a59b2d5cf3
Marco Bonomo <marco.bonomo@gmail.com>
2017-05-17 23:43:23 +0200

    add some Vue to the Mix!

---
+++ app/assets/js/components/Example.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>now you're ready to build an awesome app</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        ready() {
+            console.log('Component ready.')
+        }
+    }
+</script>
+
